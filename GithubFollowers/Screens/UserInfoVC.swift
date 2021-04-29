4f5497d0bdd078fa0ec15fad14958eda50890609
vkuday <volkuday@hotmail.com>
2021-04-29 00:07:08 -0700

    Started coding of UserInfoVC.

---
+++ GithubFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,24 @@
+//
+//  UserInfoVC.swift
+//  GithubFollowers
+//
+//  Created by yupana on 4/28/21.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+}
