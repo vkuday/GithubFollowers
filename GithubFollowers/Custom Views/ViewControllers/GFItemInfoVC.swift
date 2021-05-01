89a8bc42c33e21c8d4e1cb6ebaedb6cdb1f2689c
vkuday <volkuday@hotmail.com>
2021-04-30 23:41:15 -0700

    Started coding GFItemInfoVC

---
+++ GithubFollowers/Custom Views/ViewControllers/GFItemInfoVC.swift
@@ -0,0 +1,19 @@
+//
+//  GFItemInfoVC.swift
+//  GithubFollowers
+//
+//  Created by yupana on 4/30/21.
+//
+
+import UIKit
+
+class GFItemInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
