7c920acbddbe741da5afc00955e0cbd99e0d8386
vkuday <volkuday@hotmail.com>
2021-05-04 22:36:37 -0700

    Completed the coding of FavoritesListVC

---
+++ GithubFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,26 @@
+//
+//  GFFollowerItemVC.swift
+//  GithubFollowers
+//
+//  Created by volkan on 5/2/21.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .following, withCount: user.following)
+        itemInfoViewTwo.set(itemInfoType: .followers, withCount: user.followers)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+    
+    override func actionButtonTapped() {
+        delegate.didTapGetFollowers(for: user)
+    }
+}
